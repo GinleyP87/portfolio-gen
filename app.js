35fdacff74be6a41eeb1e5f241bc5288bc57b250
GinleyP87 <83917798+GinleyP87@users.noreply.github.com>
2021-07-20 09:36:10 -0400

    forgot to save changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -144,7 +144,6 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
-
 
 
 // promptUser()
